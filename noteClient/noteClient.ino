8e026a23547db11d599aefb7b18129496081f02a
KinoshitaYstr <control.division.cyber.crime@gmail.com>
2018-03-02 11:01:13 +0900

    update

--- noteClient/noteClient.ino
+++ noteClient/noteClient.ino
@@ -1,7 +1,9 @@
 #include <WiFi.h>
+#include <HTTPClient.h>
 
 const char SSID[] = "POPN'STAR MASTER AP";
 const char PASSWORD[] = "1234567890";
+const char URL[] = "http://192.168.20.2/note/val";
 
 WiFiServer server(80);
 
@@ -24,13 +26,28 @@ void setup() {
   Serial.print("HTTP Server: http://");
   Serial.print(WiFi.localIP());
   Serial.println("/");
+  pinMode(4, OUTPUT);
 }
 
 void loop() {
-  WiFiClient client = server.available();
-  if (client) {
-    client.println("/note/H");
-    client.stop();
+  HTTPClient http;
+  http.begin(URL);
+  int httpCode = http.GET();
+  Serial.printf("Response: %d", httpCode);
+  Serial.println();
+  if (httpCode == HTTP_CODE_OK) {
+    String body = http.getString();
+    Serial.print("Response Body: ");
+    Serial.println(body);
+    char val = body[0];
+    if(val == '0') {
+      digitalWrite(4,LOW);
+      Serial.println("stop");
+    }else if(val == '1') {
+      digitalWrite(4,HIGH);
+      Serial.println("start");
+    }
   }
+  http.end();
+  delay(1000);
 }
-
