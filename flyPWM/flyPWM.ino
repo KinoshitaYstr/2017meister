56f3fe1dd2cef85fadb8bbd8723ad0f1776d121f
KinoshitaYstr <control.division.cyber.crime@gmail.com>
2018-03-07 16:30:34 +0900

    pwm for fly

---
+++ flyPWM/flyPWM.ino
@@ -0,0 +1,32 @@
+#include <WiFi.h>
+#include <HTTPClient.h>
+
+const char SSID[] = "POP'N STAR MASTER AP";
+const char PASSWORD[] = "1234567890";
+const char URL[] = "http://192.168.20.2/fly/val";
+
+//WiFiServer server(80);
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(4,OUTPUT);
+  pinMode(5,OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  
+  //pwmGo(90);
+  for(int i = 0;i < 100;i++) 
+  {
+    for(int j = 0;j < 10000;j++) pwmGo(i);
+  }
+}
+
+void pwmGo(int per)
+{
+  digitalWrite(4,HIGH);
+  for(int i = 0;i < 100-per;i++) digitalWrite(5,HIGH);
+  for(int i = 0;i < per;i++) digitalWrite(5,LOW);
+}
+
