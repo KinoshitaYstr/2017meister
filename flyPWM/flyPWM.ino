68dd008303d02ce4c958b35a5b4c2fe7b4f76e63
KinoshitaYstr <control.division.cyber.crime@gmail.com>
2018-03-07 17:01:32 +0900

    飛ぶやつの制御かいた

--- flyPWM/flyPWM.ino
+++ flyPWM/flyPWM.ino
@@ -3,23 +3,66 @@
 
 const char SSID[] = "POP'N STAR MASTER AP";
 const char PASSWORD[] = "1234567890";
-const char URL[] = "http://192.168.20.2/fly/val";
+const char URL_PWM[] = "http://192.168.20.2/fly/pwm/val";
+const char URL_GO[] = "http://192.168.20.2/fly/go/val";
 
-//WiFiServer server(80);
+WiFiServer server(80);
 
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(115200);
+  while(!Serial);
+
+  WiFi.begin(SSID,PASSWORD);
+  Serial.print("WiFi connecting");
+
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(100);
+  }
+
+  Serial.println(" connected");
+
+  server.begin();
+
+  Serial.print("HTTP Server: http://");
+  Serial.print(WiFi.localIP());
+  Serial.println("/");
+  
   pinMode(4,OUTPUT);
   pinMode(5,OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  
-  //pwmGo(90);
-  for(int i = 0;i < 100;i++) 
-  {
-    for(int j = 0;j < 10000;j++) pwmGo(i);
+  HTTPClient http;
+  http.begin(URL_GO);
+  int httpCode = http.GET();
+  Serial.printf("Response: %d", httpCode);
+  Serial.println();
+  if (httpCode == HTTP_CODE_OK) {
+    String body = http.getString();
+    Serial.print("Response Body: ");
+    Serial.println(body);
+    char val = body[0];
+    Serial.println(val);
+    if(val == '0') {
+      digitalWrite(4,HIGH);
+      digitalWrite(5,HIGH);
+      Serial.println("stop");
+    }else if(val == '1') {
+      http.begin(URL_PWM);
+      httpCode = http.GET();
+      Serial.printf("Response: %d", httpCode);
+      Serial.println();
+      if(httpCode == HTTP_CODE_OK) {
+        body = http.getString();
+        int pwmVal = 10*body.toInt();
+        Serial.printf("pwm : %d",pwmVal);
+        Serial.println();
+        pwmGo(pwmVal);
+      }
+    }
   }
 }
 
